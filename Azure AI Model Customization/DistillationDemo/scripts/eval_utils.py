e86e050eb045fca49953cba6c4670a47d5e1ea8d
Dave Voutila <davevoutila@microsoft.com>
2025-06-13 10:37:08 -0400

    fix tracking eval id and model name per run being fetched

--- Azure AI Model Customization/DistillationDemo/scripts/eval_utils.py
+++ Azure AI Model Customization/DistillationDemo/scripts/eval_utils.py
@@ -145,17 +145,20 @@ def display_evaluation_summary(client: openai.Client, eval_ids: list):
         print("Fetching scores...")
         print("=" * 50)
 
-        futures = []
+        futures = {} # dict of "future: (model, eval_id)" so we can easily access which model powered the run.
         with ThreadPoolExecutor(thread_name_prefix="eval-run-fetcher") as pool:
             for _, row in df.iterrows():
                 run_id = row['id']
-                model = row['model']
                 eval_id = row['eval_id']
+                future = pool.submit(get_eval_run_output_items, client, eval_id, run_id)
+                futures.update({ future: (row['model'] , eval_id)})
 
-                futures.append(pool.submit(get_eval_run_output_items, client, eval_id, run_id))
-        for r in as_completed(futures):
+        print(f"XXX created futures: {futures}")
+
+        for f in as_completed(futures.keys()):
             try:
-                scores = r.result()
+                model, eval_id = futures[f]
+                scores = f.result()
             except Exception as e:
                 print(f"exception fetching future result: {e}")
                 scores = None
